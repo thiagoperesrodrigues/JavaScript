7a4919c2b27943957745e10346295e1a398a7326
thiagoperesrodrigues <55200783+thiagoperesrodrigues@users.noreply.github.com>
2019-10-17 14:48:52 -0300

    Delete systemd-udevd-solv.sh

--- systemd-udevd-solv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo systemctl stop systemd-udevd systemd-udevd-kernel.socket systemd-udevd-control.socket
-
-sleep 5
-
-sudo systemctl start systemd-udevd systemd-udevd-kernel.socket systemd-udevd-control.socket
